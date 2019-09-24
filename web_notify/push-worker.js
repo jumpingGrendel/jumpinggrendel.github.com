2c1c762ec47cee8c9ebdbfdc8d11650955e43f25
John Greene <jumpinggrendel@gmail.com>
2019-09-24 15:49:14 -0700

    web notify

---
+++ web_notify/push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1569356064.9566429
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://res\u002D5.cloudinary.com/crunchbase\u002Dproduction/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/w4n7ive2c3mwporkodk3',
+  defaultTitle: 'Branch Test',
+  defaultActionURL: 'https://www.branch.io/',
+  appKey: 'aPvmiLsDQG6h8zFNeW4VzQ',
+  token: 'MTphUHZtaUxzRFFHNmg4ekZOZVc0VnpROk41bllSUS1SVTdhT1EzNU1peG1EbG1MR3hYU0NaSy1OZjJQUkdCT0dMWkk',
+  vapidPublicKey: 'BPE7i90IzAB5oxWmp_gYslfiuWyAwyGiT8kz4y12UWBnKwm73GTAY1SZlrUgAur0WrrhUWRXz5y3CzeYMtJ5JqQ='
+})
